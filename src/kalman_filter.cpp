ba608fbda43d21149826b4cc631d537128a5da21
aliasaswad <alias_aswad@yahoo.com>
2020-06-28 05:06:12 -0500

    UpdateEKF() for kalman_filter.cpp file

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -23,19 +23,43 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
+  /**
+   * Predict the state
+   */
   x_ = F_ * x_ ;
   MatrixXd Ft = F_.transpose();
   P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  
+  /**
+   * Update the state by using Kalman Filter equations
+   */
   VectorXd y = z - H_ * x_;
   UpdateWithY(y);
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
   /**
-   * TODO: update the state by using Extended Kalman Filter equations
+   * Update the state by using Extended Kalman Filter equations
    */
+  double px = x_(0);
+  double py = x_(1);
+  double vx = x_(2);
+  double vy = x_(3);
+
+  double rho = sqrt(px*px + py*py);
+  double theta = atan2(py, px);
+  double rho_dot = (px*vx + py*vy) / rho;
+  VectorXd h = VectorXd(3);
+  h << rho, theta, rho_dot;
+  VectorXd y = z - h;
+  while ( y(1) > M_PI || y(1) < -M_PI ) {
+    if ( y(1) > M_PI ) {
+      y(1) -= M_PI;
+    } else {
+      y(1) += M_PI;
+    }
+  }
+  UpdateWithY(y);
 }
